02f1bc041e923b1dc536ae3e8a906be5d96fd96f
Niklas Lindberg <dorkinacage@gmail.com>
2017-08-24 13:54:40 +0200

    Navigation klar

--- js/main.js
+++ js/main.js
@@ -1,32 +1,36 @@
 (function(){
-/*Togglar menyknapp och navigation små viewports*/
+
 function toggleSiteNav() {
 
 	let target = document.querySelector("#nav-icon");
 	let el = document.querySelector(".sitenav-sm"); 
-
+	let header = document.querySelector('.site-header');
 	target.addEventListener('click', function(){
 		if(el.classList.contains('sitenav-sm-show')) {
 			el.classList.remove('sitenav-sm-show');
 			target.classList.remove('open');
+			header.classList.remove('site-header-hide')
 		} else {
 			el.classList.add('sitenav-sm-show');
 			target.classList.add('open');
+			header.classList.add('site-header-hide')
 		}
 	});
 }
-/*Gömmer navigation små viewports*/
+
 function siteNavHide() {
 	
 	let target = document.getElementsByClassName("navigation-link");
 	let el = document.querySelector(".sitenav-sm"); 
 	let navBtn = document.querySelector("#nav-icon");
+	let header = document.querySelector('.site-header');
 	
 	for(var i = 0; i < target.length; i++) {
 		target[i].addEventListener('click', function(){
 			if(el.classList.contains('sitenav-sm-show')) {
 				el.classList.remove('sitenav-sm-show');
 				navBtn.classList.remove('open');
+				header.classList.remove('site-header-hide')
 			} else {
 				el.classList.add('sitenav-sm-show');
 				navBtn.classList.add('open');
@@ -34,11 +38,11 @@ function siteNavHide() {
 	  });
 	}
 }
-/*Gömmer naigation när användaren scrollar ner*/
+
 function hideNavOnScroll() {
 
 	let lastScrollTop = 0;
-	let siteNav = document.querySelector('#site-header');
+	let siteNav = document.querySelector('.site-header');
 
 	window.addEventListener("scroll", () => { 
 		if(window.pageYOffset > 150) {
