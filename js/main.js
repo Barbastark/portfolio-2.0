d942697051ed03d59a2fec338cb59cd274339d13
Hobo <hobo@kvartersgarden.local>
2017-08-28 15:11:19 +0200

    en commit

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ function toggleSiteNav() {
 	let target = document.querySelector("#nav-icon");
 	let el = document.querySelector(".sitenav-sm"); 
 	let header = document.querySelector('.site-header');
+	
+
 	target.addEventListener('click', function(){
 		if(el.classList.contains('sitenav-sm-show')) {
+			
 			el.classList.remove('sitenav-sm-show');
 			target.classList.remove('open');
 			header.classList.remove('site-header-fade')
 		} else {
+			
 			el.classList.add('sitenav-sm-show');
 			target.classList.add('open');
 			header.classList.add('site-header-fade')
