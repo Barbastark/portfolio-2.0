7fe7d67c0ecacbd8c1ee2307d1cbe86bcda594f8
Niklas Lindberg <dorkinacage@gmail.com>
2017-09-08 10:42:44 +0200

    en till commit

--- js/main.js
+++ js/main.js
@@ -1,54 +1,42 @@
 (function(){
-
+"use strict";
 function toggleSiteNav() {
 
-	let target = document.querySelector("#nav-icon");
-	let el = document.querySelector(".sitenav-sm"); 
+	let navBtn = document.querySelector('#nav-icon');
+	let nav = document.querySelector('.sitenav-sm');
+	let body = document.querySelector('body'); 
 	let header = document.querySelector('.site-header');
-	
+	let navLink = document.querySelectorAll('.navigation-link');
 
-	target.addEventListener('click', function(){
-		if(el.classList.contains('sitenav-sm-show')) {
-			
-			el.classList.remove('sitenav-sm-show');
-			target.classList.remove('open');
+	for(var i = 0; i < navLink.length; i++) {
+	  	navLink[i].addEventListener('click', () => {
+	  		body.removeAttribute('style','overflow: hidden;');
+			nav.classList.remove('sitenav-sm-show');
+			navBtn.classList.remove('open');
+			header.classList.remove('site-header-fade')
+		});
+	}
+	
+	navBtn.addEventListener('click', () => {
+		if(nav.classList.contains('sitenav-sm-show')) {
+			body.removeAttribute('style','overflow: hidden;');
+			nav.classList.remove('sitenav-sm-show');
+			navBtn.classList.remove('open');
 			header.classList.remove('site-header-fade')
 		} else {
-			
-			el.classList.add('sitenav-sm-show');
-			target.classList.add('open');
+			body.setAttribute('style','overflow: hidden;');
+			nav.classList.add('sitenav-sm-show');
+			navBtn.classList.add('open');
 			header.classList.add('site-header-fade')
 		}
 	});
 }
-
-function siteNavHide() {
-	
-	let target = document.getElementsByClassName("navigation-link");
-	let el = document.querySelector(".sitenav-sm"); 
-	let navBtn = document.querySelector("#nav-icon");
-	let header = document.querySelector('.site-header');
-	
-	for(var i = 0; i < target.length; i++) {
-		target[i].addEventListener('click', function(){
-			if(el.classList.contains('sitenav-sm-show')) {
-				el.classList.remove('sitenav-sm-show');
-				navBtn.classList.remove('open');
-				header.classList.remove('site-header-fade')
-			} else {
-				el.classList.add('sitenav-sm-show');
-				navBtn.classList.add('open');
-			}
-	  });
-	}
-}
-
 function hideNavOnScroll() {
 
 	let lastScrollTop = 0;
 	let siteNav = document.querySelector('.site-header');
 
-	window.addEventListener("scroll", () => { 
+	window.addEventListener('scroll', () => { 
 		if(window.pageYOffset > 150) {
 			let offset = window.pageYOffset; 
 			offset > lastScrollTop ? siteNav.classList.add('site-header-hidden') : siteNav.classList.remove('site-header-hidden')
@@ -56,9 +44,7 @@ function hideNavOnScroll() {
 		}
 	});
 }
-
 toggleSiteNav();
-siteNavHide();
 hideNavOnScroll();
 }());
 
